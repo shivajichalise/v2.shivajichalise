a26648471a8385666dec58453e094e16a8432c03
Shivaji Chalise <respond2shivaji@gmail.com>
2024-05-08 08:59:35 +0545

    using return key icon

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -44,7 +44,20 @@ const ContactForm = () => {
         />
       </form>
       <div className="flex justify-end items-center mt-10">
-        <p className="text-xs font-thin">Press return to submit</p>
+        <p className="text-xs font-thin flex justify-center items-center">
+          Press
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="#fff"
+            viewBox="0 0 256 256"
+            className="mx-1"
+          >
+            <path d="M184,104v32a8,8,0,0,1-8,8H99.31l10.35,10.34a8,8,0,0,1-11.32,11.32l-24-24a8,8,0,0,1,0-11.32l24-24a8,8,0,0,1,11.32,11.32L99.31,128H168V104a8,8,0,0,1,16,0Zm48-48V200a16,16,0,0,1-16,16H40a16,16,0,0,1-16-16V56A16,16,0,0,1,40,40H216A16,16,0,0,1,232,56ZM216,200V56H40V200H216Z"></path>
+          </svg>
+          to submit
+        </p>
       </div>
     </div>
   );
